40948b1735777b36754465f21f32b9aead658e8b
Hkhhy <103885180+Hkhyyy@users.noreply.github.com>
2025-03-12 16:53:16 +0900

    [feat] 맵 플레이어 생성 위치 및 상호작용 오브젝트 이벤트 추가

--- Assets/Scripts/Controller/InteractController.cs
+++ Assets/Scripts/Controller/InteractController.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class InteractController : MonoBehaviour
 {
-
+    // TODO : 퍼즐 관련 컨트롤러 추가
+    
+    // 플레이어 인풋이벤트 발생 시 실행 함수
+    public void OnInteract()
+    {
+        Debug.Log("OnInteract");
+    }
 }
--- Assets/Scripts/Controller/MapController.cs
+++ Assets/Scripts/Controller/MapController.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-
+    // 플레이어 시작위치 잡아주기
+    public Transform playerStartPos;
+    // 플레이어
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 플레이어 시작위치 잡아주기
+        player.transform.position = playerStartPos.position;
+    }
 }
