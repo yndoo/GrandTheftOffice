53ebc4a864c13fcd01c1cee1ab9914b64396e7e2
leesiyen <leesiyen02012@naver.com>
2025-03-13 19:23:39 +0900

    [chore]드래그앤드롭

--- Assets/Scripts/TestCode/ItemSlot.cs
+++ Assets/Scripts/TestCode/ItemSlot.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ItemSlot : MonoBehaviour
 {
+    public ItemData item;
+
+    public UIInventory inventory;
+
+    public int index;
+    public bool equipped;
+    public int quantity;
     // Start is called before the first frame update
     void Start()
     {
