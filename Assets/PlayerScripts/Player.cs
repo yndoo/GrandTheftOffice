5c668e848d4d1bcd41b886e7635880223695e4a1
leesiyen <leesiyen02012@naver.com>
2025-03-13 15:15:19 +0900

    [chore]캐릭터 상호작용

--- Assets/PlayerScripts/Player.cs
+++ Assets/PlayerScripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
+
+    public ItemData itemData;
+    public Action addItem;
 
     private void Awake()
     {
