5a179cc7975ba865696a8aef8c8f859cbede0c9a
leesiyen <leesiyen02012@naver.com>
2025-03-13 20:10:29 +0900

    [fix]드래그로직 수정

---
+++ Assets/Scripts/Player/Object/Drag_Drop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+
+public class Drag_Drop : MonoBehaviour
+
+{
+
+    public GameObject itemObject;
+    public PlayerController controller;
+    public Player player;
+    
+    public ItemData itemData;
+
+    public LayerMask oblayer;
+    
+     
+    public void OnMouseDrag()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit
+        }
+    }
+}
