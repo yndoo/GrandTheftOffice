8d8b3b1c8a064beaf90130aa03470e55d21ceb6f
yndoo <isabelle2k@naver.com>
2025-03-14 19:10:59 +0900

    [feat] 플레이어 픽업 시 벽뚫는 현상 수정, HeldItem 회전 기능 추가

--- Assets/Scripts/Player/DragDrop.cs
+++ Assets/Scripts/Player/DragDrop.cs
@@ -13,10 +13,14 @@ public class DragDrop : MonoBehaviour
     public Camera camera;
     public bool isHolding = false;
     public LayerMask oblayer;
+    public LayerMask cantDragLayer;
     public Rigidbody heldItem;
     public Transform holdPositon;
 
     public float pickupRange = 5f;
+    private float heldDistance;
+
+    private Coroutine RotateCoroutine;
 
     void Start()
     {
@@ -34,10 +38,11 @@ public class DragDrop : MonoBehaviour
             if (itemRB != null)
             {
                 heldItem = itemRB;
+                holdPositon.position = hit.transform.position;
+                heldDistance = Vector3.Distance(transform.position, heldItem.position);
                 heldItem.useGravity = false;
                 isHolding = true;
             }
-
         }
     }
 
@@ -60,7 +65,8 @@ public class DragDrop : MonoBehaviour
     public void Drop()
     {
         heldItem.useGravity = true ;
-        isHolding= false ;
+        heldItem.includeLayers = oblayer;
+        isHolding = false ;
         heldItem = null ;  
     }
 
@@ -70,9 +76,41 @@ public class DragDrop : MonoBehaviour
         if(isHolding == true && heldItem != null)
         {
             Vector3 targetPosition = holdPositon.position;
-            //Vector3 dir = targetPosition -heldItem.position;
-            //heldItem.velocity = dir;
+
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2)); // 벽뚫 방지
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, heldDistance, cantDragLayer))
+            {
+                targetPosition = hit.point;
+            }
+
             heldItem.transform.position = targetPosition;
+        }
+    }
+
+    public void OnHoldingRotate(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            RotateCoroutine = StartCoroutine(RotateItem());
+        }
+        else if(context.phase == InputActionPhase.Canceled)
+        {
+            if(RotateCoroutine != null)
+            {
+                StopCoroutine(RotateCoroutine);
+                RotateCoroutine = null;
+            }
+        }
+    }
+
+    IEnumerator RotateItem()
+    {
+        while(true)
+        {
+            heldItem.gameObject.transform.Rotate(Vector3.right * 50 * Time.deltaTime);
+            yield return null;
         }
     }
 }
